38689ae1be864dc30747817d53a3729e8ed869ed
Jay Padmanabhan <jay_padmanabhan@hotmail.com>
2023-05-10 14:01:12 -0500

    Perl

--- tools/sed/test.sh
+++ tools/sed/test.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/perl
-$RTSP_URL="rtsp://admin:GrandCam1913!@10.0.0.38:554";
-print "RTSP_URL:$RTSP_URL";
-# Replace the string RTSP_URL with values in the variable RTSP_URL
-#$ESCAPEDRTSPURL =~ s/!/\\\!/g ;
-#print "ESCAPEDRTSPURL:$ESCAPEDRTSPURL";
-$templateyaml=`cat ../../solutions/scissors/.deployment/manifests/kustomize/overlay/configMap.template.yaml`;
+$RTSP_URL="rtsp://admin:GrandCam1913!@10.0.0.39:554";
+$templateyaml=`cat ./configMap.template.yaml`;
 $templateyaml =~ s/RTSPURL/"$RTSP_URL"/g; 
 open(FH, '>', "configMap.yaml") or die $!;
 print FH $templateyaml;
