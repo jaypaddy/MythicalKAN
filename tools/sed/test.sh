4d529e2c6422523156f2ce59d1a21203bf751483
Jay Padmanabhan <jay_padmanabhan@hotmail.com>
2023-05-10 13:52:51 -0500

    PERL

--- tools/sed/test.sh
+++ tools/sed/test.sh
@@ -1,10 +1,11 @@
-# Description: Test script for sed.exe
-RTSPURL=`cat ./a.txt`
-# Replace the string RTSPURL with values in the variable RTSPURL
-echo $RTSPURL > rtspurl.txt
-ESCAPEDRTSPURL=`sed "s|!|\\\!|g" rtspurl.txt`
-# Escape
-sed -e "s|RTSPURL|$ESCAPEDRTSPURL|g" ./escape.txt 
-
-
-
+#!/usr/bin/perl
+$RTSP_URL="rtsp://admin:GrandCam1913!@10.0.0.38:554";
+print "RTSP_URL:$RTSP_URL";
+# Replace the string RTSP_URL with values in the variable RTSP_URL
+#$ESCAPEDRTSPURL =~ s/!/\\\!/g ;
+#print "ESCAPEDRTSPURL:$ESCAPEDRTSPURL";
+$templateyaml=`cat ../../solutions/scissors/.deployment/manifests/kustomize/overlay/configMap.template.yaml`;
+$templateyaml =~ s/RTSPURL/"$RTSP_URL"/g; 
+open(FH, '>', "configMap.yaml") or die $!;
+print FH $templateyaml;
+close(FH);
