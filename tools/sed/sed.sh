d50102c396107e59d0f033d415707d0c68d31f97
Jay Padmanabhan <jay_padmanabhan@hotmail.com>
2023-05-11 11:19:37 -0500

    HOPE

--- tools/sed/sed.sh
+++ tools/sed/sed.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 RTSPURL="rtsp://admin:GrandCam1913!@10.0.0.38:554"
+CLOUD_BLOB_CONN_STRING="ABC"
 echo $ESCAPEDRTSPURL
 sed -e "s|\"RTSP_URL\"|\"$RTSPURL\"|g" \
     ./configMap.template.yaml \
-    > ./configMap.yaml
+    > ./configMap.1.yaml
+sed -e "s|CLOUDBLOBCONNSTRING|\"$CLOUD_BLOB_CONN_STRING\"|g" \
+    ./configMap.1.yaml \
+    > ./configMap.yaml    
 cat ./configMap.yaml
